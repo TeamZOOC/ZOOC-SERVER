85ef1da3c9d2c9cd91d9f06a9dd8c29c859e2690
sunseo18 <sunseo1818@gmail.com>
2023-01-01 17:49:13 +0900

    [ADD] config 폴더 분리해서 dotenv 관리 #8

--- src/middleware/upload.ts
+++ src/middleware/upload.ts
@@ -1,19 +1,12 @@
-import { S3Client } from '@aws-sdk/client-s3';
+import config from '../config';
+import s3 from '../config/s3Config';
 import multer from 'multer';
 import multerS3 from 'multer-s3';
-
-const s3: S3Client = new S3Client({
-  region: 'ap-northeast-2',
-  credentials: {
-    accessKeyId: process.env.S3_ACCESS_KEY as string,
-    secretAccessKey: process.env.S3_SECRET_KEY as string,
-  },
-});
 
 const upload = multer({
   storage: multerS3({
     s3: s3,
-    bucket: process.env.S3_BUCKET as string,
+    bucket: config.bucketName,
     contentType: multerS3.AUTO_CONTENT_TYPE,
     acl: 'public-read',
 
