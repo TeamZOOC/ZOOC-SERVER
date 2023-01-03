1a87437b52abbe98461a7648da845c56fb0b5444
dltjdn <hana40004@naver.com>
2023-01-03 23:34:01 +0900

    [FIX] userController test용으로 바꿈  #10

--- src/controller/userController.ts
+++ src/controller/userController.ts
@@ -2,9 +2,9 @@ import { Request, Response } from 'express';
 import userService from '../service/userService';
 
 const signInKakao = async (req: Request, res: Response) => {
-  const headers = req.headers['authorization'];
-  const kakaoToken: string | undefined = headers?.split(' ')[1];
-  //const kakaoToken = 'lrtqU2I-F20rF6-ChgSXkbY4ZjFi6-qijDKfmaLpCiolkQAAAYV330xt';
+  //const headers = req.headers['authorization'];
+  //const kakaoToken: string | undefined = headers?.split(' ')[1];
+  const kakaoToken = 'lrtqU2I-F20rF6-ChgSXkbY4ZjFi6-qijDKfmaLpCiolkQAAAYV330xt';
 
   const accessToken = await userService.signInKakao(kakaoToken);
 
