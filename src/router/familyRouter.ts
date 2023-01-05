30b1618b220fe7359712ff4a8deba2d326a42f7e
sunseo18 <sunseo1818@gmail.com>
2023-01-05 15:31:26 +0900

    [ADD] jwt auth 추가 #22

--- src/router/familyRouter.ts
+++ src/router/familyRouter.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
 import { familyController } from '../controller';
+import auth from '../middleware/auth';
 const router: Router = Router();
 
 //? GET family
-router.get('/', familyController.getUserFamily);
+router.get('/', auth, familyController.getUserFamily);
 
 //? GET family/mypage
 router.get('/mypage', familyController.getMypage);
