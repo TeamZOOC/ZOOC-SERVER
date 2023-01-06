45ba09e51b41d837546d5af06b99558015812feb
sunseo18 <sunseo1818@gmail.com>
2023-01-07 06:58:59 +0900

    [FEAT] post /user/profile 라우터 구현 #15

--- src/router/userRouter.ts
+++ src/router/userRouter.ts
@@ -1,7 +1,9 @@
 import { Router } from 'express';
 import userController from '../controller/userController';
+import upload from '../middleware/upload';
 const router: Router = Router();
 
 router.post('/kakao/signin', userController.signInKakao);
+router.post('/profile', upload.single('file'), userController.patchUserProfile);
 
 export default router;
