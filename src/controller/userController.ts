44b4c09e818376e1ca1e444a4a20607a7f0dcbf6
sunseo18 <sunseo1818@gmail.com>
2023-01-07 06:54:01 +0900

    [FEAT] 사용자 프로필 사진 & 닉네임 수정하기 컨트롤러 코드 작성 #15

--- src/controller/userController.ts
+++ src/controller/userController.ts
@@ -1,4 +1,6 @@
 import { Request, Response } from 'express';
+import { rm, sc } from '../constants';
+import { fail, success } from '../constants/response';
 import userService from '../service/userService';
 
 const signInKakao = async (req: Request, res: Response) => {
@@ -11,8 +13,40 @@ const signInKakao = async (req: Request, res: Response) => {
   return res.status(200).json({ jwtToken: jwtToken });
 };
 
+const patchUserProfile = async (req: Request, res: Response) => {
+  const { userId, nickName } = req.body;
+  const is_photo = req.query.photo;
+
+  try {
+    // 사진 존재할 시 사진 & 닉네임 수정
+    if (is_photo) {
+      const image: Express.MulterS3.File = req.file as Express.MulterS3.File;
+      const { location } = image;
+
+      const data = userService.patchUserPhotoAndNickName(
+        +userId,
+        location,
+        nickName
+      );
+
+      return res
+        .status(200)
+        .send(success(sc.OK, rm.UPDATE_USER_PROFILE_SUCCESS, data));
+    }
+  } catch (error) {
+    return res
+      .status(sc.INTERNAL_SERVER_ERROR)
+      .send(fail(sc.INTERNAL_SERVER_ERROR, rm.INTERNAL_SERVER_ERROR));
+  }
+
+  // 사진 존재하지 않을 시
+
+  // 닉네임 수정 (photo false)
+};
+
 const userController = {
   signInKakao,
+  patchUserProfile,
 };
 
 export default userController;
