20da4dfbed0facdeec1616ed8cd63b30461f18c0
dltjdn <hana40004@naver.com>
2023-01-07 05:34:42 +0900

    [FEAT] 기록 작성하기 Controller 구현 #26

--- src/controller/recordController.ts
+++ src/controller/recordController.ts
@@ -3,6 +3,7 @@ import { rm, sc } from '../constants';
 import { fail, success } from '../constants/response';
 import recordService from '../service/recordService';
 
+//? 완료하지 않은 미션 전체 조회
 const getMission = async (req: Request, res: Response) => {
   try {
     //const userId: number = req.body.userId;
@@ -23,6 +24,7 @@ const getMission = async (req: Request, res: Response) => {
   }
 };
 
+//? 모든 펫 조회
 const getAllPet = async (req: Request, res: Response) => {
   try {
     const familyId = req.params.familyId;
@@ -41,8 +43,32 @@ const getAllPet = async (req: Request, res: Response) => {
   }
 };
 
+//? 기록 작성하기
+const createRecord = async (req: Request, res: Response) => {
+  try {
+    //const userId: number = req.body.userId;
+
+    const familyId = req.params.familyId;
+    if (!familyId)
+      return res
+        .status(sc.BAD_REQUEST)
+        .send(fail(sc.BAD_REQUEST, rm.BAD_REQUEST));
+
+    const data = await recordService.createRecord(1, +familyId);
+    return res
+      .status(sc.OK)
+      .send(success(sc.OK, rm.CREATE_RECORD_SUCCESS, data));
+  } catch (error) {
+    console.error(error);
+    return res
+      .status(sc.INTERNAL_SERVER_ERROR)
+      .send(fail(sc.INTERNAL_SERVER_ERROR, rm.INTERNAL_SERVER_ERROR));
+  }
+};
+
 const recordController = {
   getMission,
   getAllPet,
+  createRecord,
 };
 export default recordController;
