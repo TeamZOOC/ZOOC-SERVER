890d969ed146ebee19f5b142f2a420f03b51107a
sunseo18 <sunseo1818@gmail.com>
2023-01-10 07:36:30 +0900

    [FEAT] alarm 라우터 추가 #46

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,10 +3,12 @@ import userRouter from './userRouter';
 import familyRouter from './familyRouter';
 import recordRouter from './recordRouter';
 import { auth } from '../middleware';
+import alarmRouter from './alarmRouter';
 const router: Router = Router();
 
 router.use('/user', userRouter);
 router.use('/family', auth, familyRouter);
 router.use('/record', recordRouter);
+router.use('/alarm', alarmRouter);
 
 export default router;
