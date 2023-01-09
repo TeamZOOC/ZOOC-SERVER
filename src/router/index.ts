ae40723151963f59c718ec885ba820bcd31161c8
dltjdn <hana40004@naver.com>
2023-01-09 10:25:18 +0900

    [FEAT] 댓글 작상하기 Router 구현 #41

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,10 +2,12 @@ import { Router } from 'express';
 import userRouter from './userRouter';
 import familyRouter from './familyRouter';
 import recordRouter from './recordRouter';
+import commentRouter from './commentRouter';
 const router: Router = Router();
 
 router.use('/user', userRouter);
 router.use('/family', familyRouter);
 router.use('/record', recordRouter);
+router.use('/comment', commentRouter);
 
 export default router;
