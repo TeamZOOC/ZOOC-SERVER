25784a22215b2ebbb33133381597bbfe1f274fb9
dltjdn <hana40004@naver.com>
2023-01-12 17:35:28 +0900

    [FIX] yarn.lock 삭제 #50

--- src/service/index.ts
+++ src/service/index.ts
@@ -2,3 +2,4 @@ export { default as userService } from './userService';
 export { default as familyService } from './familyService';
 export { default as recordService } from './recordService';
 export { default as commentService } from './commentService';
+export { default as alaramService } from './alarmService';
