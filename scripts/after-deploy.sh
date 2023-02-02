e98e2a9f1e7321de017b6ac47c7dfeafa223ff3d
sunseo18 <sunseo1818@gmail.com>
2023-02-03 03:33:20 +0900

    [FIX] prisma generate 누락 수정

--- scripts/after-deploy.sh
+++ scripts/after-deploy.sh
@@ -4,6 +4,6 @@ REPOSITORY=/home/ubuntu/build
 cd $REPOSITORY
 
 sudo /usr/bin/yarn
-sudo /usr/bin/pm2 delete dist 
+sudo /usr/bin/yarn prisma generate 
 sudo /usr/bin/pm2 start dist
 sudo /usr/bin/pm2 logs
