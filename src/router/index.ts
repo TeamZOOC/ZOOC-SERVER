393fb0e3badf65f9a4a030e7271a7085ca705bab
sunseo18 <sunseo1818@gmail.com>
2023-02-09 00:21:50 +0900

    [FEAT] 이미지 관련 라우터 생성 #91

--- src/router/index.ts
+++ src/router/index.ts
@@ -5,6 +5,7 @@ import recordRouter from './recordRouter';
 import commentRouter from './commentRouter';
 import { auth } from '../middleware';
 import alarmRouter from './alarmRouter';
+import imageRouter from './imageRouter';
 const router: Router = Router();
 
 router.use('/user', userRouter);
@@ -12,5 +13,6 @@ router.use('/family', auth, familyRouter);
 router.use('/record', auth, recordRouter);
 router.use('/alarm', auth, alarmRouter);
 router.use('/comment', auth, commentRouter);
+router.use('/image', imageRouter);
 
 export default router;
