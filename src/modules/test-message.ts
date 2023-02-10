fbe8463e7968899581d921ab0078b4574c242d84
sunseo18 <sunseo1818@gmail.com>
2023-02-10 23:42:08 +0900

    [CHORE] 슬랙 웹훅 메시지 형식 변경 #92

--- src/modules/test-message.ts
+++ src/modules/test-message.ts
@@ -21,9 +21,9 @@ const slackMessage = (
   error: any,
   uid?: number
 ): string => {
-  return `[에러] [${method}] ${originalUrl} ${
-    uid ? `[유저아이디]: ${uid}` : 'req.user 없음'
-  } 
+  return `[에러] 
+  [${method}] ${originalUrl} 
+  ${uid ? `[유저아이디]: ${uid}` : 'req.user 없음'} 
   ${error.message}`;
 };
 
