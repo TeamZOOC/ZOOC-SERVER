2f410f13b67c4768631767b0fbd96fb3acee2b14
sunseo18 <sunseo1818@gmail.com>
2023-02-10 16:33:45 +0900

    [ADD] webhook 메시지 수정

--- src/modules/test-message.ts
+++ src/modules/test-message.ts
@@ -1,9 +1,8 @@
 import Slack from 'slack-node';
-const webhookUri =
-  'https://hooks.slack.com/services/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'; // Webhook URL
+import config from '../config';
 
 const slack = new Slack();
-slack.setWebhook(webhookUri);
+slack.setWebhook(config.SLACK_WEBHOOK_URI);
 
 const sendWebhook = async (message: string) => {
   slack.webhook(
@@ -24,7 +23,8 @@ const slackMessage = (
 ): string => {
   return `[에러] [${method}] ${originalUrl} ${
     uid ? `[유저아이디]: ${uid}` : 'req.user 없음'
-  } ${JSON.stringify(error)}`;
+  } 
+  ${error}`;
 };
 
 const webhook = { sendWebhook, slackMessage };
