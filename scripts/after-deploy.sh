436188cc72ce4116f4c32959b387103611070e8f
Jun Seo <78674565+sunseo18@users.noreply.github.com>
2023-02-17 17:04:31 +0900

    [FIX] after-deploy.sh db 변경사항 pull 누락 수정

--- scripts/after-deploy.sh
+++ scripts/after-deploy.sh
@@ -4,6 +4,7 @@ REPOSITORY=/home/ubuntu/build
 cd $REPOSITORY
 
 sudo /usr/bin/yarn
+sudo /usr/bin/yarn run db:pull
 sudo /usr/bin/yarn prisma generate 
 sudo /usr/bin/pm2 start dist
 # sudo /usr/bin/pm2 logs
