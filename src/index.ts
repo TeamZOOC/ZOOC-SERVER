d107182d6b0be07f76af2169bfe9127ef196873b
sunseo18 <sunseo1818@gmail.com>
2023-03-04 01:19:00 +0900

    [CHORE] 푸시 알림 테스트 코드 #101

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,28 @@ app.use(express.json()); // express 에서 request body를 json 으로 받아오
 
 app.use('/', router);
 
+//? 푸시 알림 테스트
+const message = {
+  android: {
+    data: {
+      title: 'ZOOC',
+      body: '[작성자 닉네임]의 새로운 페이지에 [푸시알림대상 유저 닉네임]님도 한 마디 남겨주세요!',
+    },
+  },
+  apns: {
+    payload: {
+      aps: {
+        contentAvailable: true,
+        alert: {
+          title: 'ZOOC',
+          body: '[작성자 닉네임]의 새로운 페이지에 [푸시알림대상 유저 닉네임]님도 한 마디 남겨주세요!',
+        },
+      },
+    },
+  },
+  tokens: [],
+};
+
 //* 에러 핸들링
 app.use((error: any, req: Request, res: Response, next: NextFunction) => {
   console.error(error);
