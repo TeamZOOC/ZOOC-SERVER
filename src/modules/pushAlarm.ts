34b9701aa467d781719260a5b82bfaea311bae84
sunseo18 <sunseo1818@gmail.com>
2023-03-12 13:53:42 +0900

    [CHORE] fcm 관련 정보 객체로 넣어주는 테스트 취소 #101

--- src/modules/pushAlarm.ts
+++ src/modules/pushAlarm.ts
@@ -3,26 +3,16 @@ import { MulticastMessage } from 'firebase-admin/lib/messaging/messaging-api';
 import config from '../config';
 
 const firebaseKey = {
-  // type: config.fcmType,
-  // project_id: config.fcmProjectId,
-  // private_key_id: config.fcmPrivateKeyId,
-  // private_key: config.fcmPrivateKey,
-  // client_email: config.fcmClientEmail,
-  // client_id: config.fcmClientId,
-  // auth_uri: config.fcmAuthUrl,
-  // token_uri: config.fcmTokenUri,
-  // auth_provider_x509_cert_url: config.fcmAuthProviderX509CertUrl,
-  // client_x509_cert_url: config.fcmClientX509CertUrl,
-  type: config.fcmPrivateKey.type,
-  project_id: config.fcmPrivateKey.project_id,
-  private_key_id: config.fcmPrivateKey.private_key_id,
-  private_key: config.fcmPrivateKey.private_key,
-  client_email: config.fcmPrivateKey.client_email,
-  client_id: config.fcmPrivateKey.client_id,
-  auth_uri: config.fcmPrivateKey.auth_uri,
-  token_uri: config.fcmPrivateKey.token_uri,
-  auth_provider_x509_cert_url: config.fcmPrivateKey.auth_provider_x509_cert_url,
-  client_x509_cert_url: config.fcmPrivateKey.client_x509_cert_url,
+  type: config.fcmType,
+  project_id: config.fcmProjectId,
+  private_key_id: config.fcmPrivateKeyId,
+  private_key: config.fcmPrivateKey,
+  client_email: config.fcmClientEmail,
+  client_id: config.fcmClientId,
+  auth_uri: config.fcmAuthUrl,
+  token_uri: config.fcmTokenUri,
+  auth_provider_x509_cert_url: config.fcmAuthProviderX509CertUrl,
+  client_x509_cert_url: config.fcmClientX509CertUrl,
 } as admin.ServiceAccount;
 
 const app = admin.initializeApp({
